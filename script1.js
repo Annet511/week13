1332dfd9b5424492761a30eb3936c55c8d76832d
Annet511 <chydyshko@mail.ru>
2022-05-09 03:17:02 +0300

    add arrow

--- script1.js
+++ script1.js
@@ -1,4 +1,10 @@
-function getName(){
-    let name = prompt('What is your name?');
-    alert (`Your name is ${name}`);
-}
+// function getName(){
+//     let name = prompt('What is your name?');
+//     alert (`Your name is ${name}`);
+// }
+ 
+
+let getName = () => {
+    let name = prompt ('What is your name?');
+    alert('Your name is ' + name)
+    }
