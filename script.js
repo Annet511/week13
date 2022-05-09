ce307370fe5c2037ad9ae46fd6129774e59be97c
Annet511 <chydyshko@mail.ru>
2022-05-09 18:30:38 +0300

    add slider

--- script.js
+++ script.js
@@ -29,26 +29,91 @@
 // }
 
 
-function plus() {
-    let num1 = prompt("Введите первое число");
-    let num2 = prompt("Введите второе число");
-    alert(Number(num1) + Number(num2)); 
+// function plus() {
+//     let num1 = prompt("Введите первое число");
+//     let num2 = prompt("Введите второе число");
+//     alert(Number(num1) + Number(num2)); 
+// }
+
+// function minus() {
+//     let num1 = prompt("Введите первое число");
+//     let num2 = prompt("Введите второе число");
+//     alert(Number(num1) - Number(num2)); 
+// }
+
+// function times() {
+//     let num1 = prompt("Введите первое число");
+//     let num2 = prompt("Введите второе число");
+//     alert(Number(num1) * Number(num2)); 
+// }
+
+// function devide() {
+//     let num1 = prompt("Введите первое число");
+//     let num2 = prompt("Введите второе число");
+//     alert(Number(num1) / Number(num2)); 
+// }
+
+
+
+
+function plus(){
+    let num1 = document.getElementById('num1').value;
+    num1 = Number(num1);
+
+    let num2 = document.getElementById('num2').value;
+    num2 = Number(num2);
+    let result = num1 + num2;
+    document.getElementById('out').innerHTML = result;
+}
+function minus(){
+    let num1 = document.getElementById('num1').value;
+    num1 = Number(num1);
+
+    let num2 = document.getElementById('num2').value;
+    num2 = Number(num2);
+    let result = num1 - num2;
+    document.getElementById('out').innerHTML = result;
+}
+function times(){
+    let num1 = document.getElementById('num1').value;
+    num1 = Number(num1);
+
+    let num2 = document.getElementById('num2').value;
+    num2 = Number(num2);
+    let result = num1 * num2;
+    document.getElementById('out').innerHTML = result;
+}
+function divide(){
+    let num1 = document.getElementById('num1').value;
+    num1 = Number(num1);
+
+    let num2 = document.getElementById('num2').value;
+    num2 = Number(num2);
+    let result = num1 / num2;
+    document.getElementById('out').innerHTML = result;
 }
 
-function minus() {
-    let num1 = prompt("Введите первое число");
-    let num2 = prompt("Введите второе число");
-    alert(Number(num1) - Number(num2)); 
+
+function changeColor (sender) {
+sender.classList.add ('changecolor');
 }
 
-function times() {
-    let num1 = prompt("Введите первое число");
-    let num2 = prompt("Введите второе число");
-    alert(Number(num1) * Number(num2)); 
-}
 
-function devide() {
-    let num1 = prompt("Введите первое число");
-    let num2 = prompt("Введите второе число");
-    alert(Number(num1) / Number(num2)); 
+let offset = 0; // смещение от левого края
+const sliderLine = document.querySelector ('.slider-line');
+
+document.querySelector ('.slider-next').addEventListener ('click', function(){
+offset = offset + 256; //offset += 256
+if (offset > 512) {
+    offset = 0;
 }
+sliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector ('.slider-prev').addEventListener ('click', function(){
+offset = offset - 256; //offset -= 256
+if (offset < 0) {
+    offset = 512;
+}
+sliderLine.style.left = -offset + 'px';
+});
